8b5d95a0a18027fac28fc1c2f086e0f984bdd149
pajadam <pajadam@linux.pl>
2017-06-07 12:03:36 +0200

    Fix little configuration mistake

--- competition-osdev-08-pajadam/scripts/run.py
+++ competition-osdev-08-pajadam/scripts/run.py
@@ -10,4 +10,4 @@ def run():
         raise Exception("Missing disk image.\nYou should run 'python manage.py build' first.")
 
     print "Running bochs..."
-    call('bochs -f "../conf/osdev.bochsrc"')
+    call('bochs -f "../conf/osdev.bochsrc"', shell=True)
